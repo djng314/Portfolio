6ab9d9c03c2e5164f1791d76f01aaae62952f0ea
djng314 <64079593+djng314@users.noreply.github.com>
2022-06-29 09:40:42 +0100

    resize main component

--- components/Main.tsx
+++ components/Main.tsx
@@ -6,7 +6,7 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Main = () => {
   return (
-    <div id='home' className='w-full h-screen text-center'>
+    <div id='home' className='pt-10 w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='text-sm tracking-widest text-gray-600'>
